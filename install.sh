f7dcd41dbd960f14e590e9f8e54529218c365eda
Jaime García <elgambiter@gmail.com>
2016-01-23 12:34:18 +0000

    Make install.sh file

---
+++ install.sh
@@ -0,0 +1,23 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+#Install rainbow www.github.com/bqlabs/rainbow
+
+sudo apt-get install python-dev libzmq3-dev avahi-daemon
+
+# Disable avahi on init
+sudo update-rc.d -f avahi-daemon remove
+
+sudo pip install pyrainbow
+
+
+#Install slic3r
+
+sudo apt-get install slic3r
+
+
+#Install the Sunrise server
+
+sudo mkdir /opt/sunrise
+sudo cp -r ./src/* /opt/sunrise/
+
+sudo sed -i -e '$i \python /opt/sunrise/sunrise_server.py \n' /etc/rc.local
