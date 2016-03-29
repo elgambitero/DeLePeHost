4e228fc83440271472597e2d83844bdbc420b2d0
Jaime Garcia <elgambitero@gmail.com>
2016-03-29 10:22:40 +0000

    modify install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 #Install rainbow www.github.com/bqlabs/rainbow
 
-sudo apt-get install python-dev libzmq3-dev avahi-daemon --fix-missing
+sudo apt-get install -y python-dev libzmq3-dev avahi-daemon --fix-missing
 
 # Disable avahi on init
 sudo update-rc.d -f avahi-daemon remove
 
-sudo pip install pyrainbow
+sudo pip install -y pyrainbow
 
 
 #Install slic3r
 
-sudo apt-get install slic3r --fix-missing
+sudo apt-get install -y slic3r --fix-missing
+
+
 
 #Uninstall old Sunrise server
 sudo rm -r /opt/sunrise
@@ -21,4 +23,25 @@ sudo rm -r /opt/sunrise
 sudo mkdir /opt/sunrise
 sudo cp -r ./src/* /opt/sunrise/
 
-sudo sed -i -e '$i \sudo chmod 777 "/dev/ttyAMA0"\n\r /usr/bin/python /opt/sunrise/sunrise_server.py \n' ~/.config/lxsession/LXDE-pi/autostart
+#Install ajstark's openvg libraries
+cd $DIR
+cd ..
+git clone https://github.com/elgambitero/openvg.git
+cd openvg
+sudo apt-get install -v libjpeg8-dev indent libfreetype6-dev ttf-dejavu-core
+make library
+sudo make install
+
+#Install display python module
+cd $DIR
+cd src/DLP_Printer/Projector
+sudo python setup.py install
+
+
+sudo sed -i -e '$i /usr/bin/python /opt/sunrise/sunrise_server.py \n' /etc/rc.local
+
+#Put USER into dialout group
+sudo usermod -a -G dialout $USER
+
+#Reboot system
+sudo reboot
